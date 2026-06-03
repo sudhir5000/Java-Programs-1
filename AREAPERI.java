775e7a53d6e8a0678e4e7c93fdbf426049dca143
sudhir5000 <vsudhir5000@gmail.com>
2026-06-03 11:11:26 +0530

    Add files via upload

---
+++ AREAPERI.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int l=sc.nextInt();
+		int b=sc.nextInt();
+		
+		int area=l*b;
+		int peri=2*(l+b);
+		
+		if(area>peri)
+		{
+		System.out.println("Area");
+		System.out.println(area);
+		}
+	     else if (peri>area)
+		{
+		    System.out.println("Peri");
+		    System.out.println(peri);
+		}
+		
+		else
+		{
+		    System.out.println("Equal");
+		    
+		}
+
+	}
+}
